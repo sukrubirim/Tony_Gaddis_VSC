954821cf15c34c03663b5da72ffc5df363101f89
Sukru Birim <sukru.birim@gmail.com>
2025-02-07 16:48:17 +0300

    q4

---
+++ a4.py
@@ -0,0 +1,25 @@
+def create_number_file():
+    import random
+    file_name=input("Enter a file name: ")
+    num_file=open(file_name,"w")
+    for line in range(random.randint(1,10)):
+        num_file.write(f"{random.randint(1,100)}\n")
+    num_file.close()
+    return file_name
+
+def display_several_number_of_line(file_name):
+    MAX_ORDER_LINE=5
+    count=1
+    num_file=open(file_name,"r")
+    line=num_file.readline()
+    while line!="" and count<=5:
+        print(f"#{count} ",line,end="")
+        count+=1
+        line=num_file.readline()
+    num_file.close()
+
+def main():
+    file_name=create_number_file()
+    display_several_number_of_line(file_name)
+
+main()
